c259c895fb06733262ab5ae511a82db1ca4178fb
Annie Chen <annieche@andrew.cmu.edu>
2015-10-07 22:57:59 -0400

    Authentication added

--- Present/ViewController.swift
+++ Present/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Parse
+import LocalAuthentication
 
 class ViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.authenticateUser()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -69,6 +71,126 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    //TouchID Authentication Below
+    
+    
+    
+    func authenticateUser() {
+        
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        let reasonString = "Authentication required to access this application"
+        
+        
+        
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            context.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: {(success, policyError) -> Void in
+                
+                if success {
+                    
+                    print ("Authentication Successful")
+                    
+                }
+                    
+                else {
+                    
+                    switch policyError!.code{
+                        
+                    case LAError.SystemCancel.rawValue:
+                        
+                        print("Authentication was cancelled by system")
+                        
+                    case LAError.UserCancel.rawValue:
+                        
+                        print("Authentication was cancelled by user")
+                        
+                    case LAError.UserFallback.rawValue:
+                        
+                        print("User selected to enter password")
+                        
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            
+                            self.showPasswordAlert()
+                            
+                        })
+                        
+                    default:
+                        
+                        print("Authentication failed!")
+                        
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            
+                            self.showPasswordAlert()
+                            
+                        })
+                        
+                    }
+                    
+                }
+                
+            })
+            
+        }
+            
+        else{
+            
+            print(error?.localizedDescription)
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                
+                self.showPasswordAlert()
+                
+            })
+            
+        }
+        
+    }
+    func showPasswordAlert() {
+        
+        let alertController = UIAlertController(title: "Touch ID Password", message: "Please enter your password", preferredStyle: .Alert)
+        
+        
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Cancel) { (action) -> Void in
+            
+            if let textField = alertController.textFields?.first as! UITextField?{
+                
+                if textField.text == "password"{
+                    
+                    print("Authentication Successful")
+                    
+                }
+                    
+                else{
+                    
+                    self.showPasswordAlert()
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        
+        alertController.addAction(defaultAction)
+        
+        
+        
+        alertController.addTextFieldWithConfigurationHandler{ (textField) -> Void in
+            
+            textField.placeholder = "Password"
+            
+            textField.secureTextEntry = true
+            
+        }
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
     }
 
 }
