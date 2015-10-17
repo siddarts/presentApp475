b9170f46e9637735fb9dc1334204da77f869a1dd
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-10-17 19:29:59 -0400

    able to show classes

--- Present/CoursesTableViewController.swift
+++ Present/CoursesTableViewController.swift
@@ -12,10 +12,19 @@ import Parse
 class CoursesTableViewController: UITableViewController {
     
     var currentUser = PFUser.currentUser()!
+    var courses:[AnyObject] = []
+    
+    override func viewWillAppear(animated: Bool) {
+        courses = getCourses()
+//        courses = ["test1", "2", "3"]
+//        for course in courses {
+//            print(course["name"] as! String)
+//        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getCourses()
+//        courses = ["test1", "2", "3"]
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -32,46 +41,120 @@ class CoursesTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return courses.count
     }
     
-    func getCourses() {
-        let userEvents = PFQuery(className: "User_Event")
-        userEvents.whereKey("userId", equalTo: currentUser.objectId!)
-        userEvents.findObjectsInBackgroundWithBlock {
-            (objects: [PFObject]?, error: NSError?) -> Void in
-            
-            if error == nil {
-                // The find succeeded.
-                print("Successfully retrieved \(objects!.count) userEvents.")
-                // Do something with the found objects
-                if let userEvents = objects! as? [PFObject] {
-                    for userEvent in userEvents {
-                        print(userEvent["eventId"])
-                    }
-                }
-            } else {
-                // Log details of the failure
-                print("Error: \(error!) \(error!.userInfo)")
-            }
+    func getCourses() -> [AnyObject] {
+        var allCourses : [AnyObject] = []
+        let allUserEvents = PFQuery(className: "User_Event")
+        print(allUserEvents)
+        allUserEvents.whereKey("userId", equalTo: currentUser)
+        
+        var userEvents : [AnyObject] = []
+        
+        do {
+            userEvents = try allUserEvents.findObjects() as [PFObject]
+        } catch _ {
+            userEvents = []
         }
+    
+        for ue in userEvents {
+        
+//            print(ue)
+            
+            
+        //            query.includeKey("name")
+//            query.whereKey("objectId", equalTo: ue["eventId"])
+//            print (ue)
+            let courseId = ue["eventId"]!?.objectId
+            print(courseId)
+//            allCourses.append(course!)
+            
+            let course = PFQuery(className: "Event")
+//            course.includeKey("name")
+//            course.whereKey("objectId", equalTo: courseId!)
+            
+//            allCourses.append(ue["eventId"]!! as! PFObject)
+
+            course.whereKey("objectId", equalTo: courseId!!)
+
+            do {
+                let courseObject = try course.getFirstObject()
+//                print(courseObject)
+                allCourses.append(courseObject)
+            } catch _ {
+                let courseObject = "error"
+            }
+            
+        }
+        
+        print(allCourses)
+        return allCourses
+        
+        
+//        
+//        userEvents.findObjectsInBackgroundWithBlock {
+//            (objects: [PFObject]?, error: NSError?) -> Void in
+//            
+//            if error == nil {
+//                // The find succeeded.
+//                print("Successfully retrieved \(objects!.count) userEvents.")
+//                // Do something with the found objects
+//                if let userEvents = objects! as? [PFObject] {
+//                    for userEvent in userEvents {
+////                        print(userEvent)
+////                        print(userEvent["eventId"])
+//                        allCourses.insert(userEvent["eventId"], atIndex: 0)
+//                        print(allCourses)
+////                        print(userEvent["eventId"])
+////                        let course = PFQuery(className: "Event")
+////                        course.getObjectInBackgroundWithId(userEvent["eventId"].objectId!!) {
+////                            (courses: PFObject?, error: NSError?) -> Void in
+//////                            print(courses?["name"])
+//////                            allCourses.append(courses!)
+////                        }
+//                        
+//                    }
+//                }
+//                return allCourses
+//            } else {
+//                // Log details of the failure
+//                print("Error: \(error!) \(error!.userInfo)")
+//            }
+//        }
+//        return allCourses
         
     }
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
 
-        // Configure the cell...
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "CoursesTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CoursesTableViewCell
+        let course = courses[indexPath.row]
+//        print (course)
+        let courseName = course["name"] as! String
+//        let courseId = ue["eventId"]!!.objectId as String
+//        let course = PFQuery(className: "Event")
+//        course.whereKey("objectId", equalTo: courseId)
+//        let courseName = course.findObjects()
+        
+//        let courseName = course.objectId as String!
+        cell.courseNameLabel.text = courseName
+//        if let label = cell.courseNameLabel {
+//            print("in here")
+//            label.text = course as? String
+//
+//        }
+//        cell.courseNameLabel.text = course as? String
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
